7ed4c5acc7e72e8d9b70c47af71b3f29745ba6b6
Paul Sztajer <coding@pdyxs.wtf>
2025-07-11 19:54:44 +1000

    script update

--- install-scripts.sh
+++ install-scripts.sh
@@ -2,14 +2,14 @@ source ./env
 
 
 for repo in $REPOS; do
-    ../platform-tools/adb push $repo/*.lua $SCRIPTS_DIR
+    adb push $repo/*.lua $SCRIPTS_DIR
 done
 
 for repo in $DUP; do
     for script in $repo/*.lua; do
         file=${script##*/}
-        ../platform-tools/adb push $script $SCRIPTS_DIR/${file%.*}2.lua
-        ../platform-tools/adb push $script $SCRIPTS_DIR/${file%.*}3.lua
-        ../platform-tools/adb push $script $SCRIPTS_DIR/${file%.*}4.lua
+        adb push $script $SCRIPTS_DIR/${file%.*}2.lua
+        adb push $script $SCRIPTS_DIR/${file%.*}3.lua
+        adb push $script $SCRIPTS_DIR/${file%.*}4.lua
     done
 done
--- rm-scripts.sh
+++ rm-scripts.sh
@@ -5,7 +5,7 @@ TO_RM="ru samples community"
 for repo in $TO_RM; do
     cd $repo
     for script in *.lua; do
-        ../../platform-tools/adb shell rm -rf $SCRIPTS_DIR/$script
+        adb shell rm -rf $SCRIPTS_DIR/$script
     done
     cd ..
 done
